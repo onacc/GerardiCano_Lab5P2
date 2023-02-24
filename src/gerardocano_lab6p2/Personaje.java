fdf287ac699ca81e680113e48e2bb41be458f53e
Gerardo Cano <gcano639@gmail.com>
2023-02-24 14:02:42 -0600

    commi1 1 creacion de menu prinicipal

---
+++ src/gerardocano_lab6p2/Personaje.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package gerardocano_lab6p2;
+
+import javax.swing.tree.DefaultMutableTreeNode;
+
+/**
+ *
+ * @author gcano
+ */
+public class Personaje {
+    private String nombre,poder,debilidad;
+    private double fuerza,hp,fisica,mental;
+    private DefaultMutableTreeNode universo;
+    
+}
